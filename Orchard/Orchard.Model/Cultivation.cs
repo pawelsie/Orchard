24ef0c2aa863d82246ee77dbc56b5dcc43964bc1
Pawel S <psierpniak@interia.pl>
2018-12-09 22:29:01 +0100

    Work on new structure

---
+++ Orchard/Orchard.Model/Cultivation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orchard.Model
+{
+    /// <summary>
+    /// Uprawa
+    /// </summary>
+    public class Cultivation
+    {
+        
+    }
+
+  
+}
